df577028967dacf51b582410ac63649e0d5dc295
ltroya-as <luis.troya@allansolutions.com>
2025-06-26 16:07:29 -0500

    feat: add isr demo

---
+++ pages/no-isr.vue
@@ -0,0 +1,24 @@
+
+
+<template>
+  <div>
+    <h1>Welcome to My Vue App</h1>
+    <div v-if="time">
+      <p>Current Time: {{ time.datetime }}</p>
+      <p>Timezone: {{ time.timezone }}</p>
+    </div>
+    <p v-else>Loading time...</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface TimeResponse {
+  datetime: string;
+  timezone: string;
+  utc_datetime: string;
+  utc_offset: string;
+}
+
+// Using Nuxt's built-in composable for data fetching
+const { data: time } = await useFetch<TimeResponse>('http://worldtimeapi.org/api/timezone/UTC');
+</script>
